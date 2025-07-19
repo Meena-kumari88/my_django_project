3392e4cd8eb434994917bc0e25eba1e94afb8a83
Meena kumari <meenasahu8827@gmail.com>
2025-07-19 18:34:08 +0530

    Created model for users

--- my_project/my_django_project/admin.py
+++ my_project/my_django_project/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'name', 'role', 'is_staff' )
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+
+    fieldsets= (
+        (None, {'fields': ('email','name','role', 'password')}),
+        ('Permission', {'fields':('is_staff','is_admin', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes':('wide',),
+            'fields':('email','name', 'role', 'password1', 'password2')
+        }),
+    )
+admin.site.register(User, UserAdmin)    
--- my_project/my_django_project/models.py
+++ my_project/my_django_project/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, role, password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(
+                email = email,
+                name = name,
+                role = role
+              )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, email, name, role, password=None):
+        user = self.create_user(
+            email=email,
+            name= name,
+            role= role, 
+            password= password
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=100)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'role']
+    
+    def __str__(self):
+        return self.name   
+    
+    def has_perm(self, perm, obj = None):
+        return self.is_superuser or self.is_staff
+    
+    def has_module_perms(self, app_label):
+        return self.is_superuser or self.is_staff

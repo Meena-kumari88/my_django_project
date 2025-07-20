843c92a5e47b7555ec5d9244c2849846b8b8d1fa
Meena kumari <meenasahu8827@gmail.com>
2025-07-20 15:39:35 +0530

    Created template folder and html files and created view to display users and user detail

--- my_project/my_django_project/views.py
+++ my_project/my_django_project/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import User
+from django.http import Http404
 
 # Create your views here.
+
+def user_list(request):
+    users = User.email.all()
+    
+    return render(
+        request,
+        'my_django_project\user\list.html',
+         {'users':users}
+    )
+
+def user_detail(request, id):
+    try:
+        user = User.email.get(id=id)
+    except User.DoesNotExist:
+        raise Http404("No User found.")
+    return render (request, 'my_django_project\user\detail.html', {'user':user})    

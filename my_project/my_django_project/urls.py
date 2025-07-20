004be5149da22cb2a65438d9a234b44699f020aa
Meena kumari <meenasahu8827@gmail.com>
2025-07-20 17:35:32 +0530

    Created urls and view for hello, users, new_user, and user_detail

--- my_project/my_django_project/urls.py
+++ my_project/my_django_project/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'my_django_project'
 
 urlpatterns = [
-    path('', views.user_list, name= 'user_list'),
-    path('<int:id>/', views.user_detail, name='user_detail'),
+    path('hello/', views.hello, name= 'hello'),
+    path('users/', views.user_list, name= 'users'),
+    path('new_user/', views.new_user, name='new_user'),
+    path('users/<int:id>/', views.user_detail, name='user_detail'),
 ]
--- my_project/my_django_project/views.py
+++ my_project/my_django_project/views.py
@@ -1,21 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
-from django.http import Http404
+from django.http import Http404, HttpResponse
+from .forms import UserForm
 
 # Create your views here.
 
+def hello(request):
+    return HttpResponse("Hello World")
+
 def user_list(request):
-    users = User.email.all()
+    users = User.objects.all()
     
     return render(
         request,
-        'my_django_project\user\list.html',
+        'my_django_project/user/users.html',
          {'users':users}
     )
 
 def user_detail(request, id):
     try:
-        user = User.email.get(id=id)
+        user = User.objects.get(id=id)
     except User.DoesNotExist:
         raise Http404("No User found.")
-    return render (request, 'my_django_project\user\detail.html', {'user':user})    
+    return render (request, 'my_django_project/user/detail.html', {'user':user}) 
+
+def new_user(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        role = request.POST.get('role')
+        
+        if name and email and role:
+            User.objects.create(name=name, email=email, role=role)
+            return redirect('my_django_project:users')
+    return  render(request, 'my_django_project/user/new_user.html')    
+
